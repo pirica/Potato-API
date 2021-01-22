4859d8a742bbde1aaff283df4f6d7e922eb0a643
Potato <z4bakedpotato@gmail.com>
2021-01-21 21:46:41 -0800

    Added note

--- authorized.php
+++ authorized.php
@@ -1,4 +1,8 @@
 <?php
+	/**
+		Note:	This is a sample file for what authorized.php contains.
+				The actual authorized.php file will have more authorized users.
+	*/
 	$authorizedUsers = [
 		"5172d8e597914d9589c1ee183aa7d8fe"
 	];
